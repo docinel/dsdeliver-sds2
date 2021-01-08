10a1c63e7be8ca5d4fc61e824ec5fa2299f5b763
docinel <docinel@gmail.com>
2021-01-08 10:03:16 -0300

    Add Footer

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
+import Footer from './Footer';
 import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
   return (
       <>
-      <Navbar />
-      <Home />
+      <Navbar/>
+      <Home/>
       </>
   );
 }
