a6e9ad037c9fd98cd855355239a762810ce47f7a
docinel <docinel@gmail.com>
2021-01-08 13:29:49 -0300

    Starting the ProductList

---
+++ front-web/src/Orders/ProductsList.tsx
@@ -0,0 +1,17 @@
+import ProductsCard from "./ProductCard";
+
+function ProductsList(){
+    return(
+       <div className="orders-list-container">
+       <div className="orders-list-items">
+           <ProductsCard/>
+           <ProductsCard/>
+           <ProductsCard/>
+           <ProductsCard/>
+
+       </div>
+       </div>      
+    )
+}
+
+export default ProductsList;
